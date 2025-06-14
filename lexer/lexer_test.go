b3ebba8236b66fd319e44554e8f458bfefa4e99a
0xdeadbad <matheus.dasilva.garcias@gmail.com>
2025-06-14 06:03:30 +0000

    start basic lexer state testing

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -1,3 +1,29 @@
 package lexer_test
 
+import (
+	"context"
+	"testing"
+	"yal/lexer"
+)
+
 // TODO: write actual tests
+
+func TestMultilineComment(t *testing.T) {
+	t.Run("Test multiLineCommentState()", func(t *testing.T) {
+		src := ` /* aaa
+                    bbbb
+                    ccc
+                */`
+
+		l := lexer.NewLexer(context.Background(), string(src))
+
+		tokens, err := l.Scan()
+		if err != nil {
+			panic(err)
+		}
+
+		if len(tokens) != 1 && tokens[0].TokenType != lexer.Eof {
+			t.Errorf("%s got %+v\n", "there should be only one token here: Eof", tokens)
+		}
+	})
+}
--- lexer/util_test.go
+++ lexer/util_test.go
@@ -47,3 +47,83 @@ func TestIsBase16(t *testing.T) {
 		}
 	}
 }
+
+func TestIsBase8(t *testing.T) {
+	numMap := map[string]int64{
+		"010":   010,
+		"020":   020,
+		"01000": 01000,
+		"075":   075,
+	}
+
+	for k, v := range numMap {
+		num, err := strconv.ParseInt(k, 8, 0)
+		if err != nil {
+			t.Error(err)
+		}
+
+		if num != v {
+			t.Errorf("string key converted is different from int value: %d != %d\n", num, v)
+		}
+
+		for i := range k {
+			if !IsBase8(k[i]) {
+				t.Errorf("IsBase8 failed: IsBase8(%s) = %T\n", k, false)
+			}
+		}
+
+		rn, err := rand.Int(rand.Reader, big.NewInt(0x07FFFFFFFFFFFFFF))
+		if err != nil {
+			t.Error(err)
+		}
+
+		for j := 0; j < 10; j++ {
+			srn := fmt.Sprintf("%o", rn.Int64())
+			for i := range srn {
+				if !IsBase8(srn[i]) {
+					t.Errorf("IsBase8 failed: IsBase8(%s) = %T\n", srn, false)
+				}
+			}
+		}
+	}
+}
+
+// func TestIsBase2(t *testing.T) {
+// 	numMap := map[string]int64{
+// 		"0b10110110": 0b10110110,
+// 		"0b10001101": 0b10001101,
+// 		"0b1000":     0b1000,
+// 		"0b0":        0b0,
+// 	}
+
+// 	for k, v := range numMap {
+// 		num, err := strconv.ParseInt(strings.ReplaceAll(k, "0b", ""), 2, 0)
+// 		if err != nil {
+// 			t.Error(err)
+// 		}
+
+// 		if num != v {
+// 			t.Errorf("string key converted is different from int value: %d != %d\n", num, v)
+// 		}
+
+// 		for i := range k {
+// 			if !IsBase2(k[i]) {
+// 				t.Errorf("IsBase2 failed: IsBase2(%s) = %t\n", k, false)
+// 			}
+// 		}
+
+// 		rn, err := rand.Int(rand.Reader, big.NewInt(0x07FFFFFFFFFFFFFF))
+// 		if err != nil {
+// 			t.Error(err)
+// 		}
+
+// 		for j := 0; j < 10; j++ {
+// 			srn := strings.ReplaceAll(fmt.Sprintf("%b", rn.Int64()), "0b", "")
+// 			for i := range srn {
+// 				if !IsBase2(srn[i]) {
+// 					t.Errorf("IsBase2 failed: IsBase2(%s) = %T\n", srn, false)
+// 				}
+// 			}
+// 		}
+// 	}
+// }
