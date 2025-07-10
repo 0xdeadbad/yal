8095c7ed0335d2a6ebb1ed231c433df1b1184a2b
0xdeadbad <matheus.dasilva.garcias@gmail.com>
2025-07-10 00:34:45 +0000

    change tests structure

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -8,12 +8,27 @@ import (
 
 // TODO: write actual tests
 
-func TestMultilineComment(t *testing.T) {
+func TestComments(t *testing.T) {
 	t.Run("Test multiLineCommentState()", func(t *testing.T) {
 		src := ` /* aaa
                     bbbb
                     ccc
                 */`
+
+		l := lexer.NewLexer(context.Background(), string(src))
+
+		tokens, err := l.Scan()
+		if err != nil {
+			panic(err)
+		}
+
+		if len(tokens) != 1 && tokens[0].TokenType != lexer.Eof {
+			t.Errorf("%s got %+v\n", "there should be only one token here: Eof", tokens)
+		}
+	})
+
+	t.Run("Test oneLineCommentState()", func(t *testing.T) {
+		src := `// aaaa bbbb ccc`
 
 		l := lexer.NewLexer(context.Background(), string(src))
 
